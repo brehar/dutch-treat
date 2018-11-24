35d05d2b3ac6a1499d9e4db9ae11334ca35e378f
Brett Hartman <brett.hartman@mac.com>
2018-11-24 13:13:00 -0800

    Initial commit

---
+++ DutchTreat/Startup.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DutchTreat
+{
+	public class Startup
+	{
+		public void ConfigureServices(IServiceCollection services)
+		{
+		}
+
+		public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+		{
+			app.UseDefaultFiles();
+			app.UseStaticFiles();
+		}
+	}
+}
